e7e8f2794b16b679f6edc685fef39949472175d1
NotThankYou <eugene.vlast@gmail.com>
2026-05-26 10:36:09 -0600

    Game Manager Set Up

--- game_manager.gd
+++ game_manager.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 signal money_amount(amount:float)
 
-@export var money_start : float
+@export var money_start := 100
+
 
 func _ready() -> void:
 	money_amount.emit(money_start)
--- ui.gd
+++ ui.gd
@@ -1,7 +1,8 @@
 extends CanvasLayer
 
-@onready var quota : Label = $MarginContainer/HBoxContainer3/HBoxContainer2/Panel/Label
+@onready var quota : Label = $MarginContainer/HBoxContainer3/HBoxContainer/Panel/Label
 
 
 func _ready() -> void:
+	quota.text = str(GameManager.money_start)
 	pass
