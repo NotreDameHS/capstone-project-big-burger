e750ccbe9248820ce2f5ea44f7e1d89f9de61aec
NotThankYou <eugene.vlast@gmail.com>
2026-05-26 10:15:39 -0600

    Connecting UI with GameManager

--- game_manager.gd
+++ game_manager.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+signal money_amount(amount:float)
+
+@export var money_start: float
+
+func _ready() -> void:
+	money_amount.emit(money_start)
---
+++ ui.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+@onready var quota : Label = $MarginContainer/HBoxContainer3/HBoxContainer2/Panel/Label
+
+
+func _ready() -> void:
+	pass
