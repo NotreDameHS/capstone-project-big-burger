583b547eabfff708a5856bc614c05d786d2526b6
NotThankYou <eugene.vlast@gmail.com>
2026-05-26 10:23:57 -0600

    Update game_manager.gd

--- game_manager.gd
+++ game_manager.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-signal money_amount(amount:float)
+signal money_change(amount:float)
 
 @export var money_start: float
 
 func _ready() -> void:
-	money_amount.emit(money_start)
+	money_change.emit(money_start)
